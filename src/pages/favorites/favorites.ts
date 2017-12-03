a7b20a8f02354394cd1ce12b0326a1bffb855eaa
Luc Schlicher <luc.schlicher@cgi.com>
2017-12-03 16:07:50 +0100

    Add a button on "favorites" page

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-favorites',
@@ -7,7 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class FavoritesPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: AlertController) {
 
+  }
+
+
+  doAlert() {
+    let alert = this.navCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 }
